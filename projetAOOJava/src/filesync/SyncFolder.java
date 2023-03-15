2b248553cbb51b32df2f04bec39df54240fb8e05
corentinleandre <corentinleandre+github@gmail.com>
2023-03-15 15:49:39 +0100

    created path variable

--- projetAOOJava/src/filesync/SyncFolder.java
+++ projetAOOJava/src/filesync/SyncFolder.java
@@ -3,6 +3,10 @@ package filesync;
 public class SyncFolder {
 
 	private String name = "Nouveau Dossier";
+	private static String localpath = "";
 	
+	public SyncFolder(String name) {
+		this.name = name;
+	}
 	
 }
